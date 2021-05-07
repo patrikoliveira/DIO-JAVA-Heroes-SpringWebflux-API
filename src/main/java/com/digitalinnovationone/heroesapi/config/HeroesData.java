c9e7af75486f29c6c242ed780dfa56d4d6e956ac
Patrik Oliveira <patrikro.oliveira@gmail.com>
2021-05-07 18:27:12 -0300

    feat: model + definição de properties + refactor

--- src/main/java/com/digitalinnovationone/heroesapi/config/HeroesData.java
+++ src/main/java/com/digitalinnovationone/heroesapi/config/HeroesData.java
@@ -7,11 +7,9 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.model.*;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
 
 import static com.digitalinnovationone.heroesapi.constants.HeroesConstant.ENDPOINT_DYNAMO;
 import static com.digitalinnovationone.heroesapi.constants.HeroesConstant.REGION_DYNAMO;
